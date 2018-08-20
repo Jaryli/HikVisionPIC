a4b262e08e145bbe8621840d8a54da6c9ac30904
lsw8569013 <lsw8569013@qq.com>
2018-08-20 16:01:59 +0800

    上传demo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HikVisionPIC
 海康卫视实时预览和抓图功能
+
+6.0 以上权限自己申请一下 
+
+海康威视Android studio版 直接代码中改变参数就可以使用
+
+此demo 参考： https://github.com/helloword1/HikVisionDemo
---
+++ app/src/main/java/com/xx/hikvisiondemo/LogUtil.java
@@ -0,0 +1,21 @@
+package com.xx.hikvisiondemo;
+
+import android.util.Log;
+
+
+
+
+/**
+ * @author lsw
+ * @date 2018/4/20
+ */
+public class LogUtil {
+
+
+    public static void e(String message) {
+
+            Log.e("lsw",message);
+
+
+    }
+}
